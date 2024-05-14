f34acbc4ba3f19f40f282159acb93765fd35a566
Josh Henderson <joshuahenderson031@gmail.com>
2024-05-14 10:07:29 -0700

    first neural network commit

--- p1.py
+++ p1.py
@@ -1,8 +1,6 @@
-import sys
-import numpy as np
-import matplotlib
+inputs = [1.2, 5.1, 2.1]
+weights = [3.1, 2.1, 8.7]
+bias = 3
 
-
-print("Python:", sys.version)
-print("Numpy:", np.__version)
-print("Matplotlibn: ", matplotlib.__version__)
+output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + bias
+print(output)
